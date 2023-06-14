867ecf7c0a808f637c927d6318e9d18955b3d109
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-06-13 23:25:59 -0600

    Add deleteNode function

--- script.js
+++ script.js
@@ -42,9 +42,65 @@ function insert(val, root) {
     return root;
 }
 
+function deleteNode(val, root) {
+    if (root === null) {
+        return root;
+    }
+    if (val < root.data) {
+        root.left = deleteNode(val, root.left);
+        return root;
+    }
+    else if (val > root.data) {
+        root.right = deleteNode(val, root.right);
+        return root;
+    }
+
+    if (root.left === null) {
+        let temp = root.right;
+        delete root;
+        return temp;
+    }
+    else if (root.right === null) {
+        let temp = root.left;
+        delete root;
+        return temp;
+    }
+    else {
+        let successorParent = root;
+        let successor = root.right;
+        while (successor.left !== null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successorParent !== root) {
+            successorParent.left = successor.right;
+        }
+        else {
+            successorParent.right = successor.right;
+        }
+        root.data = successor.data;
+        delete successor;
+        return root;
+    }
+}
+/*
 const tree1 = Tree([1,2,4,5,6,7]);
 prettyPrint(tree1.root);
 
 insert(3,tree1.root);
 
 prettyPrint(tree1.root);
+
+deleteNode(3, tree1.root);
+prettyPrint(tree1.root);
+
+deleteNode(1, tree1.root);
+prettyPrint(tree1.root);
+
+deleteNode(6, tree1.root);
+prettyPrint(tree1.root);
+*/
+const tree2 = Tree([1, 3, 6, 9, 13, 17, 22, 27, 33, 39]);
+prettyPrint(tree2.root);
+deleteNode(13, tree2.root);
+prettyPrint(tree2.root);
